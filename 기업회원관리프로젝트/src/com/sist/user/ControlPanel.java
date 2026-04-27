08d19b198aa8abb0001332e0a979483e37327bc7
chaijewon <vcandjava@nate.com>
2026-04-27 12:20:39 +0900

    1111

---
+++ 기업회원관리프로젝트/src/com/sist/user/ControlPanel.java
@@ -0,0 +1,13 @@
+package com.sist.user;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+// 화면 변경 
+public class ControlPanel extends JPanel{
+    public ControlPanel()
+    {
+    	setBackground(Color.cyan);
+    }
+}
---
+++ 기업회원관리프로젝트/src/com/sist/user/HomePanel.java
@@ -0,0 +1,5 @@
+package com.sist.user;
+// 메인 화면 
+public class HomePanel {
+
+}
